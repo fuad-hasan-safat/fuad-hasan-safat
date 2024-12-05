face64742d63366fd95abca446609c1808eff469
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-12-05 13:17:42 +0600

    cards color changed

--- components/InformationCard.tsx
+++ components/InformationCard.tsx
@@ -10,9 +10,9 @@ interface CardProps {
 export default function InformationCard({ title, subtitleInfo, cgpa, children }: CardProps) {
   return (
     <div
-      className={`w-full max-w-[650px] bg-gray-800 hover:bg-gray-950 rounded-md z-[999] shadow-md shadow-slate-900 my-[25px] py-[30px] px-[20px] font-exo_2 relative ease-in-out duration-500`}
+      className={`w-full max-w-[650px] bg-white hover:bg-gray-950 rounded-md z-[999] shadow-md shadow-slate-900 my-[25px] py-[30px] px-[20px] font-exo_2 relative ease-in-out duration-500`}
     >
-      <h1 className={`text-3xl pr-[50px]`}>{title}</h1>
+      <h1 className={`text-3xl pr-[50px] text-slate-800`}>{title}</h1>
       <h2 className={`text-sm pt-[15px] text-slate-500`}>{subtitleInfo}</h2>
       <div className={`absolute top-[35px] right-[15px] text-red-400`}>
         <span>{cgpa}</span>
