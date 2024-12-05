8f9f52ddbafa1ab17247bea85bdb7dc890b4ca55
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-12-05 13:00:07 +0600

    remove key

--- components/HighlightWords.tsx
+++ components/HighlightWords.tsx
@@ -2,13 +2,11 @@ import React, { HTMLAttributes } from 'react'
 
 interface HighlightWordsProps extends HTMLAttributes<HTMLSpanElement> {
   highlightClassName?: string;
-  searchWords?: string[];
 }
 
 export default function HighlightWords({
   children,
   highlightClassName = 'text-yellow-200',
-  searchWords = [],
   ...props
 }: HighlightWordsProps) {
   return (
