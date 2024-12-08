0d01518c9b9b09fcd84b252169bf9a4fdca3b0b0
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-12-08 17:05:29 +0600

    footer added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='bg-blue-500/60 backdrop-blur-sm mb-[100px] py-[25px] p-[25px] rounded-lg'>
-      Footer
+    <div className='flex justify-center bg-blue-500/60 shadow-lg shadow-slate-50 backdrop-blur-sm mb-[100px] py-[25px] p-[25px] rounded-lg w-full'>
+      <div>
+        <p className='font-playwrite text-center text-sm text-white'>
+          &copy; {new Date().getFullYear()} Fuad Hasan Safat. All Rights Reserved.
+        </p>
+      </div>
     </div>
   )
 }
