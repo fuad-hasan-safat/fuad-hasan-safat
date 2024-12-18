f7f5df66368feb25e597e8a9033071b9defb8391
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-12-18 17:16:25 +0600

    modal design change

--- components/DialugueModal.tsx
+++ components/DialugueModal.tsx
@@ -13,7 +13,7 @@ const DialogueModal = forwardRef<HTMLDialogElement, DialogueModalProps>(
         return (
             <dialog
                 ref={ref}
-                className="w-full max-w-md backdrop-blur-sm bg-white border border-gray-300 rounded-lg shadow-lg p-6 transform transition-transform duration-300 scale-100 open:scale-100 open:opacity-100 opacity-0"
+                className="bg-white/60 opacity-0 open:opacity-100 shadow-lg backdrop-blur p-6 border rounded-lg w-full max-w-md transform transition-transform duration-300 open:scale-100 scale-100"
             >
                 <div className="text-center">
                     {/* Header */}
@@ -25,11 +25,11 @@ const DialogueModal = forwardRef<HTMLDialogElement, DialogueModalProps>(
                     <p className="mt-4 text-gray-700">{alert}</p>
 
                     {/* Buttons */}
-                    <div className="mt-6 flex justify-center gap-4">
+                    <div className="flex justify-center gap-4 mt-6">
                         {type === "Confirmation" && (
                             <button
                                 onClick={onYes}
-                                className="bg-green-600 text-white px-6 py-2 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400"
+                                className="bg-green-600 hover:bg-green-700 px-6 py-2 rounded-md focus:ring-2 focus:ring-green-400 text-white focus:outline-none"
                             >
                                 Yes
                             </button>
@@ -38,7 +38,7 @@ const DialogueModal = forwardRef<HTMLDialogElement, DialogueModalProps>(
                             onClick={() =>
                                 (ref as React.RefObject<HTMLDialogElement>).current?.close()
                             }
-                            className="bg-gray-600 text-white px-6 py-2 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400"
+                            className="bg-gray-600 hover:bg-gray-700 px-6 py-2 rounded-md focus:ring-2 focus:ring-gray-400 text-white focus:outline-none"
                         >
                             Close
                         </button>
