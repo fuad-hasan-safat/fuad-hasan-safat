a695ce64f39d44fbac08e4431749ec758fa113be
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2025-01-30 18:01:10 +0600

    animation changed

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -7,37 +7,36 @@ export default function Loader() {
             <div data-glitch="Loading..." className={styles.glitch}>Loading...</div>
         </div>
 
-
         // <div className="container">
         //     <div className="square black">
-        //         <div className="square">
+        //         <div className="square bg-pink-400">
         //             <div className="square black">
-        //                 <div className="square">
+        //                 <div className="square bg-yellow-400">
         //                     <div className="square black">
-        //                         <div className="square">
+        //                         <div className="square bg-lime-300">
         //                             <div className="square black">
-        //                                 <div className="square">
+        //                                 <div className="square bg-red-400">
         //                                     <div className="square black">
-        //                                         <div className="square">
+        //                                         <div className="square bg-blue-500">
         //                                             <div className="square black">
-        //                                                 <div className="square">
+        //                                                 <div className="square bg-emerald-300">
         //                                                     <div className="square black">
-        //                                                         <div className="square">
+        //                                                         <div className="square border-orange-400">
         //                                                             <div className="square black">
-        //                                                                 <div className="square">
+        //                                                                 <div className="square bg-lime-400">
         //                                                                     <div className="square black">
-        //                                                                         <div className="square">
+        //                                                                         <div className="square bg-cyan-600">
         //                                                                             <div className="square black">
-        //                                                                                 <div className="square">
+        //                                                                                 <div className="square border-indigo-400">
         //                                                                                     <div className="square black">
-        //                                                                                         <div className="square">
+        //                                                                                         <div className="square bg-red-400">
         //                                                                                             <div className="square black">
-        //                                                                                                 <div className="square">
+        //                                                                                                 <div className="square bg-teal-400">
         //                                                                                                     <div className="square black">
-        //                                                                                                         <div className="square">
+        //                                                                                                         <div className="square bg-blue-400">
         //                                                                                                             <div className="square black">
-        //                                                                                                                 <div className="square">
-        //                                                                                                                     <div className="square black">
+        //                                                                                                                 <div className="square bg-white">
+        //                                                                                                                     <div className="square  bg-yellow-400">
         //                                                                                                                     </div>
         //                                                                                                                 </div>
         //                                                                                                             </div>
