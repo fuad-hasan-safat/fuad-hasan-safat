3bc95b1e21743f7b7f04030f9845d4675cb40c64
fuad-hasan-safat <fuadsafat16@gmail.com>
2025-03-15 10:23:13 +0600

    export change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Inter, Dancing_Script, Exo_2, Playwrite_US_Modern, Geologica } from 'next/font/google';
@@ -50,38 +49,6 @@ const geologica = Geologica({
   weight: ["400", "500", "600", "700"]
 });
 
-export const metadata: Metadata = {
-  title: "Fuad Hasan Safat - Software Developer",
-  description: "I am Fuad Hasan, a software developer from Rowmari, Kurigram, Bangladesh. I specialize in frontend, backend, and database design, crafting innovative solutions with perseverance and adaptability.",
-  keywords: ["Fuad Hasan", "Software Developer", "Bangladesh", "Full Stack Developer", "Database Design", "Frontend Development", "Backend Development"],
-  authors: [{ name: "Fuad Hasan Safat", url: "https://fuad-hasan-safat.vercel.app/" }],
-  creator: "Fuad Hasan Safat",
-  publisher: "Fuad Hasan Safat",
-  openGraph: {
-    title: "Fuad Hasan Safat - Software Developer",
-    description: "Discover Fuad Hasan Safat, a software developer from Rowmari, Kurigram, Bangladesh, specializing in modern web technologies, backend development, and database design.",
-    url: "https://fuad-hasan-safat.vercel.app/",  // Update with your actual website URL
-    siteName: "Fuad Hasan Safat",
-    images: [
-      {
-        url: '/profilepic/fuad.jpg',
-        width: 1200,
-        height: 630,
-        alt: "Fuad Hasan Safat Profile Picture"
-      }
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@fuad_safat",  // Update with your actual Twitter handle
-    creator: "@fuad_safat", // Update with your actual Twitter handle
-    title: "Fuad Hasan Safat - Software Developer",
-    description: "Software developer from Rowmari, Kurigram, Bangladesh, specializing in modern web development and database design.",
-    images: "/profilepic/fuad.jpg"
-  },
-
-
-};
 
 export default function RootLayout({
   children,
