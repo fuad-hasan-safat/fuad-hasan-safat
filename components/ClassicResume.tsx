837312379b65832d9754b6785c71717df2c91c8b
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2025-04-29 16:41:32 +0600

    update

--- components/ClassicResume.tsx
+++ components/ClassicResume.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { motion } from "framer-motion";
-import InformationCard from "./InformationCard";
 import ClassicInformationCard from "./ClassicInformationCard";
 
 export default function ClassicResume() {
