5f02e0adfb5b8ed5e35fbf499643149165fa7ea0
fuad-hasan-safat <fuadsafat16@gmail.com>
2026-02-23 00:43:38 +0600

    upgrade to next 16

--- app/heroproviders.tsx
+++ app/heroproviders.tsx
@@ -1,11 +1,12 @@
 // app/providers.tsx
+"use client";
 
-import {HeroUIProvider} from '@heroui/react'
+import { HeroUIProvider } from "@heroui/react";
 
-export function HeroProviders({children}: { children: React.ReactNode }) {
+export function HeroProviders({ children }: { children: React.ReactNode }) {
   return (
     <HeroUIProvider>
       {children}
     </HeroUIProvider>
-  )
+  );
 }
